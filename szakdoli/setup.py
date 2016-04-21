91c25369cd228892763d88e91831e0484f30ae76
wahur666 <wahur13@inf.elte.hu>
2016-04-21 18:32:41 +0200

    cx_Freezeeeeeeee

--- szakdoli/setup.py
+++ szakdoli/setup.py
@@ -1,7 +1,9 @@
+import sys
 from cx_Freeze import setup, Executable
 
 setup(
     name='LogoRajzolo',
     version='0.1',
-    executables=[Executable("main.py")]
+    executables=[Executable("main.py")],
+    options={"build_exe": {"packages" : ["pygame", "tkinter"]}}
 )
