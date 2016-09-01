0e372ddb250e086aab39e86b7a394bc88ea9d8f7
wahur666 <wahur13@inf.elte.hu>
2016-09-02 00:35:49 +0200

    Drag N Drop

--- szakdoliV2/PythonApplication1.py
+++ szakdoliV2/PythonApplication1.py
@@ -2,11 +2,13 @@ import pygame
 import Color
 
 class Rect:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.h = 0
-        self.w = 0
+    def __init__(self,x,y,h,w, name):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
+        self.name = name
+        self.delta = 0,0
     
     def jobb(self):
         self.x += 1
@@ -14,16 +16,38 @@ class Rect:
     def bal(self):
         self.x -= 1
 
-rect = Rect()
+    def fel(self):
+        self.y += 1
 
-rect.x = 5
-rect.y = 6
-rect.h = 7
-rect.w = 8
+    def le(self):
+        self.y -= 1
 
-def drawRect(screen):
-    global rect
-    pygame.draw.rect(screen, Color.black, (rect.x, rect.y, rect.h, rect.w))
+    def drawRect(self, screen):
+        pygame.draw.rect(screen, Color.black, (self.x, self.y, self.h, self.w))
+
+    def print_name(self):
+        print self.name + " Clicked"
+
+    def isInside(self, position):
+        return self.x <= position[0] and self.x + self.h >= position[0] and self.y <= position[1] and self.y + self.w >= position[1], self.name
+
+    def deltapos(self, position):
+        return position[0]-self.x, position[1] - self.y
+
+    def drag(self, mouseposition):
+        self.x = mouseposition[0] - self.delta[0]
+        self.y = mouseposition[1] - self.delta[1]
+
+    def setDelta(self, mouseposition):
+        self.delta = self.deltapos(mouseposition)
+
+rect1 = Rect(5,5,90,20, "ALak1")
+rect2 = Rect(100,100,100,100, "ALMA")
+
+rects = []
+rects.append(rect1)
+rects.append(rect2)
+
 
 screen = pygame.display.set_mode((500,500))
 screen.fill(Color.white)
@@ -32,19 +56,41 @@ screen.fill(Color.white)
 pygame.display.init()
 pygame.display.update()
 clock = pygame.time.Clock()
-
+touching = False
+currentRect = None
 
 gameExit = False
 while not gameExit:
     pygame.display.update()
     screen.fill(Color.white)
-    drawRect(screen)
-    rect.jobb()
+    for r in rects:
+        r.drawRect(screen)
+    #rect1.drawRect(screen)
+    #rect.jobb()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.MOUSEBUTTONDOWN:
+            touching = True
+            for rect in rects:
+                insite, name = rect.isInside(event.pos)
+                if insite:
+                    print 'There is my boi boo ', name
+                    print 'Delta pos' , rect.deltapos(event.pos)
+                    currentRect = rect
+                    currentRect.setDelta(event.pos)
+                else:
+                    print 'njet komrad'
             print event.pos
+        if event.type == pygame.MOUSEMOTION:
+            if touching and currentRect:
+                #print 'DAGGING'
+                currentRect.drag(event.pos)
+        if event.type == pygame.MOUSEBUTTONUP:
+            touching = False
+            currentRect = None
+
+            print 'released'
     clock.tick(60)
         
 
