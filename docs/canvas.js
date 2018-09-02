4c9bb0527ffff10490a4fcb667f242ec17cbbf68
Imre Secodi <imre.secodi@gmail.com>
2018-09-02 21:32:05 +0200

    event bindings, and log removed

--- docs/canvas.js
+++ docs/canvas.js
@@ -10,6 +10,34 @@ import { TextIcon } from "./Drawable/TextIcon.js";
 var canvas = document.querySelector('canvas');
 
 var canvasContext = canvas.getContext("2d");
+var canvasRect = canvas.getBoundingClientRect();
+var mouseDown = false;
+
+canvas.addEventListener("mousedown", function(event) {
+    mouseDown = true;
+    console.log(event.button);
+}, false);
+
+canvas.addEventListener("mouseup", function(event) {
+    console.log(event.button);
+    mouseDown = false;
+}, false);
+
+canvas.oncontextmenu = function (e) {
+    e.preventDefault();
+};
+
+canvas.addEventListener("wheel", function(event) {
+    console.log(event.deltaY);
+}, false);
+
+canvas.addEventListener("mousemove", function(event){
+    if(mouseDown){
+        console.log(Math.round((event.clientX - canvasRect.left) / (canvasRect.right - canvasRect.left) * canvas.width),
+                    Math.round((event.clientY - canvasRect.top) / (canvasRect.bottom - canvasRect.top) * canvas.height))
+    }
+}, false);
+
 /*
 
 canvasContext.beginPath();
@@ -46,7 +74,6 @@ function mainLoop() {
     sprite.DrawObject(canvasContext, 0);
     //canvasContext.drawImage(img, 150, 150, 50, 50);
     textIcon.DrawObject(canvasContext);
-    console.log("updating");
     requestAnimationFrame(mainLoop);
 }
 
