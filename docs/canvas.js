a6fa090bcc04e64e51750dcf8fabe6b4f2ddbf4e
Szecsődi Imre <imre.secodi@gmail.com>
2018-09-18 20:32:27 +0200

    reload bug fixed

--- docs/canvas.js
+++ docs/canvas.js
@@ -3,9 +3,13 @@ import { ApplicationCore } from "./VisualLogo.js";
 var canvas = document.querySelector('canvas');
 var applicationCore = new ApplicationCore(canvas);
 
-window.onresize = function(){ location.reload(); }
+window.onresize = function () {
+    setTimeout(function () {
+        window.location.reload();
+    });
+}
 
-function mainLoop(){
+function mainLoop() {
     applicationCore.Update();
     requestAnimationFrame(mainLoop);
 }
