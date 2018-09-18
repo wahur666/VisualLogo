58ada04ebadb80701e8a387a20277a127a5bfa3e
Szecsődi Imre <imre.secodi@gmail.com>
2018-09-18 20:21:58 +0200

    small correction

--- docs/canvas.js
+++ docs/canvas.js
@@ -3,7 +3,7 @@ import { ApplicationCore } from "./VisualLogo.js";
 var canvas = document.querySelector('canvas');
 var applicationCore = new ApplicationCore(canvas);
 
-canvas.onresize = function(){ location.reload(); }
+window.onresize = function(){ location.reload(); }
 
 function mainLoop(){
     applicationCore.Update();
